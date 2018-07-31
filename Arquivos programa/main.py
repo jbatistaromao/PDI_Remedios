2c3864eec373fc2fb3365b60017e56986dfc609a
JBatista <j.batista.romao@gmail.com>
2018-07-31 11:25:14 -0300

    teste open cv

--- Arquivos programa/main.py
+++ Arquivos programa/main.py
@@ -1,1 +1,15 @@
-import numpy
+import cv2
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread("soccer.jpg")
+
+color = ('b','g','r')
+print("teste")
+cv2.imshow("teste", img)
+
+
+
+
+
